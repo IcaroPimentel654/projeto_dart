c53465ebd32285c9b46a48c41eff8cf37166f16f
fernandoabf <fernandobraga1032@gmail.com>
2025-02-28 23:11:53 -0300

    feat(main): Adiciona funcionalidade para obter o mês correspondente a um número
    - Remove a lógica de operações bancárias e validação de entrada.
    - Implementa a função obterNumeroValido() para garantir que o usuário insira um número entre 1 e 12.
    - Adiciona a função obterMes(int numero) que retorna o mês correspondente ao número fornecido.
    - Atualiza a função main() para solicitar ao usuário um número e exibir o mês correspondente.
    
    Essas alterações simplificam o código, removendo funcionalidades não utilizadas e focando na conversão de números em meses, melhorando a legibilidade e a manutenção do projeto.

--- bin/name_project.dart
+++ bin/name_project.dart
@@ -1,42 +1,59 @@
 import 'dart:io';
 
 void main() {
-  Map<String, String> operationsMethods = {
-    "deposito": "Depósito",
-    "retirada": "Retirada",
-    "transferencia": "Transferência",
-    "pagamento": "Pagamento",
-  };
+  print('Digite um número de 1 a 12 para saber o mês correspondente:');
 
-  double getOperationValue() {
-    print("\nDigite o valor da operação:");
-    String valueInput = stdin.readLineSync()!;
-    double? value = double.tryParse(valueInput);
+  int numero = obterNumeroValido();
 
-    if (value == null || value <= 0) {
-      print("\nValor inválido. Tente novamente.");
-      return getOperationValue();
+  String mes = obterMes(numero);
+  print(mes);
+}
+
+int obterNumeroValido() {
+  String? input = stdin.readLineSync();
+
+  try {
+    int numero = int.parse(input!);
+
+    if (numero < 1 || numero > 12) {
+      print('Número inválido. Tente novamente.');
+      return obterNumeroValido();
     }
 
-    return value;
+    return numero;
+  } catch (e) {
+    print('Entrada inválida. Por favor, insira um número de 1 a 12.');
+    return obterNumeroValido();
   }
+}
 
-  String getOperationMethods() {
-    print(
-      "\nDigite uma operação (deposito, retirada, transferencia, pagamento):",
-    );
-    String operationInput = stdin.readLineSync()!.toLowerCase();
-    if (!operationsMethods.containsKey(operationInput)) {
-      print("\nOperação inválida. Tente novamente.");
-      return getOperationMethods();
-    }
-    return operationInput;
+String obterMes(int numero) {
+  switch (numero) {
+    case 1:
+      return 'Janeiro';
+    case 2:
+      return 'Fevereiro';
+    case 3:
+      return 'Março';
+    case 4:
+      return 'Abril';
+    case 5:
+      return 'Maio';
+    case 6:
+      return 'Junho';
+    case 7:
+      return 'Julho';
+    case 8:
+      return 'Agosto';
+    case 9:
+      return 'Setembro';
+    case 10:
+      return 'Outubro';
+    case 11:
+      return 'Novembro';
+    case 12:
+      return 'Dezembro';
+    default:
+      return 'Número inválido. Por favor, insira um número de 1 a 12.';
   }
-
-  String operation = getOperationMethods();
-  double value = getOperationValue();
-
-  print(
-    "\nOperação escolhida: ${operationsMethods[operation]}, Valor: $value\n",
-  );
 }
