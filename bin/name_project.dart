5779dcab3b891927036c2ebc072ffc4b258113e8
fernandoabf <fernandobraga1032@gmail.com>
2025-02-28 22:31:04 -0300

    feat(main): validação de categorias de produtos
    - Melhorada a formatação da mensagem de entrada para o usuário.
    - Alterado o método de concatenação de strings para interpolação no print da categoria válida.
    
    Essas mudanças tornam o código mais legível e moderno, melhorando a experiência do usuário ao interagir com o sistema de validação de categorias.

--- bin/name_project.dart
+++ bin/name_project.dart
@@ -1,15 +1,16 @@
 import 'dart:io';
 
-//Validaçao de categorias de produtos em um sistema de estoque.
 void main() {
   List<String> categorias = ['eletronicos', 'alimentos', 'vestuario', 'livros'];
-  
-  while(true){
-    print('Digite a categoria do produto: eletronicos, alimentos, vestuario, livros\n');
+
+  while (true) {
+    print(
+      'Digite a categoria do produto: eletronicos, alimentos, vestuario, livros\n',
+    );
     String categoria = stdin.readLineSync() ?? '';
 
     if (categorias.contains(categoria.toLowerCase())) {
-      print('Categoria válida: ' + categoria);
+      print('Categoria válida: $categoria');
       break;
     } else {
       print('Categoria inválida.');
